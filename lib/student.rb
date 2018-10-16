3599ff55ba8b514b08e68244b0782d88f04247b0
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-16 11:06:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,6 @@ class Student
   end 
 
   def self.create_table
-      # creates the students table in the database
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
@@ -20,19 +19,33 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end 
+   
+   
       
-      
-    # .drop_table
+  def self.drop_table
       # drops the students table from the database
+    sql = "DROP IF EXISTS students"
+    DB[:conn].execute(sql)
+  end 
+    
+    
     # save
       # saves an instance of the Student class to the database and then sets the given students `id` attribute
     # updates a record if called on an object that is already persiste
+    
+    
     # .create
       # creates a student object with name and grade attributes
+    
+    
     # .new_from_db
       # creates an instance with corresponding attribute values
+    
+    
     # .find_by_name
       # returns an instance of student that matches the name from the DB
+    
+    
     # update
       # updates the record associated with a given instance 
 end

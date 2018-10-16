9f5aa6e5d01032110bef50e732f2d46241124820
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-16 11:31:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,11 +54,10 @@ class Student
       SELECT *
         FROM students
         WHERE name = ?
-        LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end 
+    end.first
   end 
 
   def update

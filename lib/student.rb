f30fe13fae9db78c62dfc57aeddecc75492736b5
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-16 11:00:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,24 @@
 require_relative "../config/environment.rb"
 
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-
-  .create
-    creates a student object with name and grade attributes (FAILED - 7)
-  .new_from_db
-    creates an instance with corresponding attribute values (FAILED - 8)
-  .find_by_name
-    returns an instance of student that matches the name from the DB (FAILED - 9)
-  #update
-    updates the record associated with a given instance (FAILED - 10)
+  attr_accessor :id, :name, :grade
+  
+  
+    # has a name and a grade
+    # has an id that defaults to `nil` on initialization
+    # .create_table
+      # creates the students table in the database
+    # .drop_table
+      # drops the students table from the database
+    save
+      # saves an instance of the Student class to the database and then sets the given students `id` attribute
+    # updates a record if called on an object that is already persiste
+    # .create
+      # creates a student object with name and grade attributes
+    # .new_from_db
+      # creates an instance with corresponding attribute values
+    # .find_by_name
+      # returns an instance of student that matches the name from the DB
+    update
+      # updates the record associated with a given instance 
 end

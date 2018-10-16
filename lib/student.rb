e16d5b3f961520910a2d7f775942f766e4d93d84
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-16 11:20:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,12 +47,13 @@ class Student
     student
   end 
     
-  def self.new_from_db
+  def self.new_from_db(row)
       # creates an instance with corresponding attribute values
     new_student = Student.new(name, grade)
-    
-    
-    
+    new_student.id    = row[0]
+    new_student.name  = row[1]
+    new_student.grade = row[2]
+    new_student
   end
     
     # .find_by_name
